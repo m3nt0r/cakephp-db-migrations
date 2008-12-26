9619e47e5ebc35228f4c3824b88e509e4a5ef884
Kjell Bublitz <mail@m3nt0r.de>
2008-12-26 06:18:03 +0100

    using 'fixed' in uuid_format

--- vendors/shells/migrate.php
+++ vendors/shells/migrate.php
@@ -112,6 +112,7 @@ class MigrateShell extends Shell
     var $uuid_format = array(
         'type'    => 'text',
         'notnull' => true,
+        'fixed' => true,
         'length'  => 36
     );
     
